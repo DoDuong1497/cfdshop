5561223fca89b2dfad32a94b67facebca16b2a21
IT-FE-Do-Duong <it3@lamians.com>
2021-12-13 09:27:41 +0700

    add scss and fix footer

---
+++ src/components/footer/FooterTop.jsx
@@ -0,0 +1,9 @@
+const FooterTop = () => {
+    return (
+        <div className="footer-top">
+            Footer Top
+        </div>
+    )
+}
+
+export default FooterTop;
---
+++ src/components/footer/index.jsx
@@ -0,0 +1,15 @@
+
+import FooterTop from "./FooterTop";
+import "./style.scss"
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <FooterTop />
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
