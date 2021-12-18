ca823a99f680121c385e210e02f6c79e75e3a96a
Chung Tu <tuthanhtrung2000@gmail.com>
2021-12-19 02:59:44 +0700

    Chung / product-default - homepage update

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import BannerSection from './bannerSection'
+import Productwcate from './productwcate'
 import './style.scss'
 
 export default function Home() {
     return (
         <div className='container'>
             <BannerSection/>
+            <Productwcate header='Best selling products'/>
+            <Productwcate header='Best from Farmers'/>
         </div>
     )
 }
