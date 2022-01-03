c389e22daf53f25f5959ba6996c07ffffaddfc11
Chung Tu <tuthanhtrung2000@gmail.com>
2022-01-03 21:56:28 +0700

    fix route

--- src/layout/mainlayout/index.jsx
+++ src/layout/mainlayout/index.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import Footer from '../../components/footer';
 import Header from '../../components/header';
 import Nav from '../../components/nav';
+import {Outlet } from "react-router-dom";
 
-export default function MainLayout({children}) {
+export default function MainLayout() {
     return (
         <>
         <Header/>
         <Nav/>
-        {children}
+        <Outlet/>
         <Footer/>
         </>
     )
